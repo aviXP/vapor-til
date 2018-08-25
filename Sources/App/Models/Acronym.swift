a69b993ac55abd19aff808200fcd64e07e072e42
Arjan Vissers <arjan@extendas.com>
2018-08-25 17:46:16 +0200

    first commit

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,31 @@
+import Vapor
+import FluentSQLite
+
+// It’s good practice to mark classes final, where possible, as it provides a performance benefit.
+// Models must conform to 'Codable'
+final class Acronym: Codable {
+    var id: Int?
+    var short: String
+    var long: String
+    
+    init(short: String, long:String) {
+        self.short = short
+        self.long = long
+    }
+}
+
+//extension Acronym: Model {
+//    // a named alias of an existing type
+//    typealias Database = SQLiteDatabase
+//    typealias ID = Int
+//
+//    public static var idKey: IDKey = \Acronym.id
+//}
+
+// Aboove code can be improved further with SQLiteModel:
+
+extension Acronym: SQLiteModel {}
+extension Acronym: Migration {}
+// Vapor provides Content, a wrapper around Codable, which allows you to convert models and other data between various formats.
+extension Acronym: Content {}
+
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,10 +6,18 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    // Register a new route at /api/acronyms that accepts a POST request and returns Future<Acronym>. It returns the acronym once it’s saved.
+    router.post("api", "acronyms") { req -> Future<Acronym> in
+        return try req.content.decode(Acronym.self)         // Decode the request’s JSON into an Acronym model using Codable...
+        .flatMap(to: Acronym.self) { acronym in             // ...this returns a Future<Acronym> so it uses a flatMap(to:) to extract the acronym when the decoding is complete.
+            return acronym.save(on: req)                    // Save the model using Fluent. This returns Future<Acronym> as it returns the model once it’s saved.
+        }
+    }
 
     // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+//    let todoController = TodoController()
+//    router.get("todos", use: todoController.index)
+//    router.post("todos", use: todoController.create)
+//    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
